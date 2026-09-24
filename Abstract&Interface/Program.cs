52aeba47f84513c45746649164d116535c52d66b
mohamedSayedBayoumy <mandobobo12345@gmail.com>
2026-09-24 23:53:32 +0300

    Add project files.

---
+++ Abstract&Interface/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Abstract_Interface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
